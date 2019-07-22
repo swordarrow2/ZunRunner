bbfc654125999620aea8b1669d2ef54e10cbd265
swordarrow2 <2856986197@qq.com>
2019-07-23 00:32:03 +0800

    补了一些ins 改了几个错误

--- mainCode/src/com/InsProcess/Ins.java
+++ mainCode/src/com/InsProcess/Ins.java
@@ -67,22 +67,22 @@ public class Ins {
     }
 
     public Ins ifxGoto(String judge, LoopFlag loopFlag) {
-        stringBuilder.append("if ").append(judge).append(" ").append(loopFlag.getName()).append(" @ 0;\n");
+        stringBuilder.append("if ").append(judge).append(" goto ").append(loopFlag.getName()).append(" @ 0;\n");
         return this;
     }
 
     public Ins ifxGotoAt(String judge, LoopFlag loopFlag, int at) {
-        stringBuilder.append("if ").append(judge).append(" ").append(loopFlag.getName()).append(" @ ").append(at).append(";\n");
+        stringBuilder.append("if ").append(judge).append(" goto ").append(loopFlag.getName()).append(" @ ").append(at).append(";\n");
         return this;
     }
 
     public Ins unlessxGoto(String judge, LoopFlag loopFlag) {
-        stringBuilder.append("unless ").append(judge).append(" ").append(loopFlag.getName()).append(" @ 0;\n");
+        stringBuilder.append("unless ").append(judge).append(" goto ").append(loopFlag.getName()).append(" @ 0;\n");
         return this;
     }
 
     public Ins unlessxGotoAt(String judge, LoopFlag loopFlag, int at) {
-        stringBuilder.append("unless ").append(judge).append(" ").append(loopFlag.getName()).append(" @ ").append(at).append(";\n");
+        stringBuilder.append("unless ").append(judge).append(" goto ").append(loopFlag.getName()).append(" @ ").append(at).append(";\n");
         return this;
     }
 
@@ -161,21 +161,21 @@ public class Ins {
     public String transfer(String name) {
         VarType varType = typeMap.get(name);
         if (varType == VarType.floatVar) {
-            return "_ff " + name;
+            return "_ff %" + name;
         }
         if (varType == VarType.intVar) {
-            return "_SS " + name;
+            return "_SS $" + name;
         }
         throw new EclException(varTypeUnkonwn);
     }
 
 
     public String transferIntToFloat(String name) {
-        return "_fS " + name;
+        return "_fS $" + name;
     }
 
     public String transferFloatToInt(String name) {
-        return "_Sf " + name;
+        return "_Sf %" + name;
     }
 
     public Ins _600() {
@@ -200,22 +200,11 @@ public class Ins {
         return this;
     }
 
-    public Ins _602(int form, int color) {
-        stringBuilder.append(lineStart);
-        stringBuilder.append(String.format("ins_602(%d, %d, %d)", dan, form, color));
-        stringBuilder.append(lineEnd);
-        return this;
-    }
-
     public Ins _602(String form, String color) {
         stringBuilder.append(lineStart);
         stringBuilder.append(String.format("ins_602(%d, %s, %s)", dan, form, color));
         stringBuilder.append(lineEnd);
         return this;
-    }
-
-    public Ins _603(float offsetX, float offsetY) {
-        return _603(offsetX + "f", offsetY + "f");
     }
 
     public Ins _603(String offsetX, String offsetY) {
@@ -225,25 +214,230 @@ public class Ins {
         return this;
     }
 
-    public Ins _604(float dir, float r) {
-        return _604(dir + "f", r + "f");
-    }
-
-    public Ins _604(String dir, String r) {
+    public Ins _604(String direct, String r) {
         stringBuilder.append(lineStart);
-        stringBuilder.append(String.format("ins_604(%d, %s, %s)", dan, dir, r));
+        stringBuilder.append(String.format("ins_604(%d, %s, %s)", dan, direct, r));
         stringBuilder.append(lineEnd);
         return this;
     }
 
-    public Ins _605(String speed, String s) {
+    public Ins _605(String speed, String slowlestSpeed) {
         stringBuilder.append(lineStart);
-        stringBuilder.append(String.format("ins_605(%d, %s, %s)", dan, speed, s));
+        stringBuilder.append(String.format("ins_605(%d, %s, %s)", dan, speed, slowlestSpeed));
         stringBuilder.append(lineEnd);
         return this;
     }
 
-
+	public Ins _606(String way, String ceng) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_606(%d, %s, %s)", dan, way, ceng));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _607(String style) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_607(%d, %s)", dan, style));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	
+	public Ins _608(String voiceOnShoot, String voiceOnChange) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_608(%d, %s, %s)", dan, voiceOnShoot, voiceOnChange));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	
+	public Ins _609(String num, String way, String inta, String intb, String floatr,String floats) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_609(%d, %s, %s, %s, %s, %s, %s)", dan, num, way, inta, intb, floatr, floats));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _610(String num, String way, String inta, String intb, String intc, String intd, String floatr, String floats, String floatm, String floatn) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_610(%d, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", dan, num, way, inta, intb, intc, intd , floatr, floats, floatm, floatn));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _611(String way, String inta, String intb, String floatr,String floats) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_609(%d, %s, %s, %s, %s, %s)", dan, way, inta, intb, floatr, floats));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _612(String way, String inta, String intb, String intc, String intd, String floatr, String floats, String floatm, String floatn) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_610(%d, %s, %s, %s, %s, %s, %s, %s, %s, %s)", dan, way, inta, intb, intc, intd , floatr, floats, floatm, floatn));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _613() {
+        stringBuilder.append(lineStart);
+        stringBuilder.append("ins_613()");
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _614(String danmakuA, String danmakuB) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_614(%s, %s)", danmakuA, danmakuB));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _615(String floatR) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_615(%s)", floatR));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _616(String floatR) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_616(%s)", floatR));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _623(String floatVarName, String floatX, String floatY) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_623(%s, %s, %s)", floatVarName, floatX, floatY));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	
+	public Ins _624(String floatA, String b, String c, String d, String e, String f, String g, String h) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_624(%d, %s, %s, %s, %s, %s, %s, %s, %s)", dan, floatA, b, c, d, e, f, g, h));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _625(String intA, String b, String c, String d, String e, String f, String g, String h) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_625(%d, %s, %s, %s, %s, %s, %s, %s, %s)", dan, intA, b, c, d, e, f, g, h));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	
+	public Ins _626(String floatAngel, String r) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_626(%d, %s, %s)", dan, floatAngel, r));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _627(String r) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_627(%d, %s)", dan, r));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _628(String floatX, String y) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_628(%d, %s, %s)", dan, floatX, y));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _629(String floatR, String intRgb) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_629(%s, %s)", floatR, intRgb));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  //int
+	public Ins _630(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_630(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _631(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_631(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _632(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_632(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _633(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_633(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _634(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_634(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  //float
+	public Ins _635(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_635(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _636(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_636(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  //int
+	public Ins _637(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_637(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _638(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_638(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _639(String a) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_639(%s)", a));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	//mode=16777216 dan=2
+	public Ins _640(String intMode, Sub sub) {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_640(%d, %s, %s)", dan, intMode, sub.getSubName()));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	  
+	public Ins _641() {
+        stringBuilder.append(lineStart);
+        stringBuilder.append(String.format("ins_642(%d)", dan));
+        stringBuilder.append(lineEnd);
+        return this;
+	  }
+	    
     @Override
     public String toString() {
         return stringBuilder.toString();
--- mainCode/src/com/InsProcess/zunRunner.java
+++ mainCode/src/com/InsProcess/zunRunner.java
@@ -12,14 +12,14 @@ public class zunRunner {
             public void run() {
                 Ins ins = testSub.ins();
                 ins._600();
-                ins._602(2, 5);
+                ins._602(toStr(2), toStr(5));
                 ins.assign(A, 43).assign(B, 64);
                 ins._603(ins.use(A), ins.use(B));
                 ins._601();
                 LoopFlag loop1 = ins.loop(9961);
                 ins._600()
-                        ._602(4, 2)
-                        ._603(85, 12)
+				  ._602(toStr(4), toStr(2))
+				  ._603(toStr(85), toStr(12))
                         ._601()
                         .gotoLoopFlag(loop1)
                         ._601(0);
@@ -34,8 +34,8 @@ public class zunRunner {
                 Ins ins = mainFront.ins();
                 ins.args(true, true, false, false)
                         ._600()
-                        ._602(64, 64)
-                        ._603(5, 2)
+                        ._602(toStr(64), toStr(64))
+				        ._603(toStr(5), toStr(2))
                         ._601()
                         ._600()
                         .assign(A, 64).assign(B, 54)
@@ -45,15 +45,23 @@ public class zunRunner {
                 LoopFlag flag1 = ins.loop(9961);
                 ins._11(testSub, ins.transfer(A), ins.transfer(B))
                         .push(4).push(5.0)
-                        ._602(ins.pop(1), ins.pop(1))
+                        ._602(ins.pop(), ins.pop())
                         ._601()
                         ._603(ins.use(E), ins.use(F))
-                        .ifxGoto("1", flag1)
+                        .ifxGoto(ins.use(A) + "--", flag1)
+						._606(ins.use(C), ins.use(D))
                         ._601(0);
             }
         });
     }
-
+	private String toStr(int i){
+	  return String.valueOf(i);
+	}
+	
+	private String toStr(float i){
+		return i+"f";
+	  }
+	  
     @Override
     public String toString() {
         return ecl.toString();
