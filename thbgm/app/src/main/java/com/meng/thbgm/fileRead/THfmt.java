ab0ea99d9c353863eca09f70bcbb4fce1b433385
swordarrow2 <2856986197@qq.com>
2019-08-22 02:09:10 +0800

    read musiccmt

--- thbgm/app/src/main/java/com/meng/thbgm/fileRead/THfmt.java
+++ thbgm/app/src/main/java/com/meng/thbgm/fileRead/THfmt.java
@@ -1,18 +1,24 @@
 package com.meng.thbgm.fileRead;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class THfmt {
     private int position = 0;
     private byte[] fileByte;
     public MusicInfo[] musicInfos;
     public String[] names;
+    private File fmt;
 
     public THfmt(File file) {
         if (!file.exists()) {
             throw new RuntimeException("file not found:" + file.getAbsolutePath());
         }
+        fmt = file;
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             fileByte = new byte[(int) file.length()];
@@ -44,6 +50,45 @@ public class THfmt {
             names[i] = name.substring(0, name.indexOf(0));
             musicInfos[i] = musicInfo;
         }
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null; //用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
+        try {
+            String str = "";
+            String str1 = "";
+            fis = new FileInputStream(new File(fmt.getParent() + "/musiccmt.txt"));
+            isr = new InputStreamReader(fis,"shift_jis");
+            br = new BufferedReader(isr);
+
+            while ((str = br.readLine()) != null) {
+                if (str.startsWith("@bgm/")) {
+                    String music = str.substring(5);
+                    music += ".wav";
+                    for (int i = 0; i < names.length; ++i) {
+                        if (music.equals(names[i])) {
+                            str = br.readLine();
+                            names[i] = str.substring(str.indexOf("  ") + 2);
+                            break;
+                        }
+                    }
+                }
+            }
+            // 当读取的一行不为空时,把读到的str的值赋给str1
+            System.out.println(str1);// 打印出str1
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到指定文件");
+        } catch (IOException e) {
+            System.out.println("读取文件失败");
+        } finally {
+            try {
+                br.close();
+                isr.close();
+                fis.close();
+                // 关闭的时候最好按照先后顺序关闭最后开的先关闭所以先关s,再关n,最后关m
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public class MusicInfo {
@@ -60,7 +105,7 @@ public class THfmt {
         public short bitsPerSample;
         public short cbSize;
         public short pad;
-    //    public int beanSize = 52;
+        //    public int beanSize = 52;
     }
 
     private short readShort() {
